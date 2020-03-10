2e969684b872dbfea8b5382c45d15c5c17f4e3a5
Danilo Batista <dbatistadsn@gmail.com>
2020-03-10 10:38:00 -0300

    Tipos e variáveis

---
+++ primeiros-passos/sintaxe-variaveis-e-fluxo/TestaPontoFlutuante.java
@@ -0,0 +1,29 @@
+public class TestaPontoFlutuante {
+
+    public static void main (String[] args){
+
+        double salario = 1039.20;
+
+        System.out.println(salario);
+        System.out.println("O valor do salário mínimo em 2020 é de R$ " + salario + ".");
+
+        /*
+        int valor = 12.5;
+        int valor2 = 0.0;
+        double teste = 125.50;
+        int valor3 = teste;
+        */
+
+        int divisao = 5 /2;
+        System.out.println(divisao);
+
+        double divisao1 = 5 /2;
+        System.out.println(divisao1);
+
+        double divisao2 = 5.0 /2;
+        System.out.println(divisao2);
+    }
+
+
+
+}
---
+++ primeiros-passos/sintaxe-variaveis-e-fluxo/TestaVariaveis.java
@@ -0,0 +1,15 @@
+public class TestaVariaveis {
+
+    public static void main (String[] args){
+
+        System.out.println("Olá, Mundo!");
+
+        int idade = 35;
+
+        System.out.println(idade);
+
+        System.out.println("Parabéns! Você tem agora " + idade + " anos.");
+
+    }
+
+}
